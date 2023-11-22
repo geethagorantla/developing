ea232dafe047a1fb7ab51531bfc3a1ab7c822835
Geetha Madhuri <geethagorantla20@gmail.com>
2023-11-22 09:12:17 +0530

    Chnages in Props

--- src/passingprops/Avatar.js
+++ src/passingprops/Avatar.js
@@ -3,6 +3,7 @@ import {getImageUrl} from './utils/imagefiles'
 
 const Avatar = ({width=300,height,person}) => {
   return (
+    
     <div>
       <img src={getImageUrl(person)} alt={person?.name} height={height} width={width}/>
     </div>
